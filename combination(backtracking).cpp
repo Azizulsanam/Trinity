28cfa6662dcd0390c8420ea94a78fb84fca4b053
Azizulsanam <54774857+Azizulsanam@users.noreply.github.com>
2019-10-25 20:59:26 +0600

    Add files via upload

---
+++ combination(backtracking).cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void permute(string a, int l, int r)
+{
+
+	if (l == r)
+		cout<<a<<endl;
+	else
+	{
+
+		for (int i = l; i <= r; i++)
+		{
+
+			swap(a[l], a[i]);
+
+
+			permute(a, l+1, r);
+
+
+			swap(a[l], a[i]);
+		}
+	}
+}
+
+
+int main()
+{
+    cout<<"enter any string or number to get the combination"<<endl;
+	string str;
+	cin>>str;
+	int n = str.size();
+	permute(str, 0, n-1);
+	return 0;
+}
+
+// if the input contains two same letter or number this program would consider them as different element
+// so there can be duplicates
